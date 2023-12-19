d9238d8c55daad5bc73f231f35dfe4c177522ea9
cconfort <113508854+csconfort1@users.noreply.github.com>
2023-12-19 16:49:49 +0000

    Add files via upload

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,10 +1,10 @@
-import { FiHome } from 'react-icons/fi';
-import { useNavigate } from 'react-router-dom';
+import {FiHome} from 'react-icons/fi';
+import {useNavigate} from 'react-router-dom';
 
-import { Button } from '../../components/Button';
-import { Footer } from '../../components/Footer';
-import { Header } from '../../components/Header';
-import { Container } from './styles';
+import {Button} from '../../components/Button';
+import {Footer} from '../../components/Footer';
+import {Header} from '../../components/Header';
+import {Container} from './styles';
 
 export function NotFound() {
   const navigate = useNavigate();
