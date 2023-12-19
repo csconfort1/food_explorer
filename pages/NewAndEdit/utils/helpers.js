a7e6837938046a395309f68fd3fa0c779a3088c4
cconfort <113508854+csconfort1@users.noreply.github.com>
2023-12-19 16:08:47 +0000

    Delete pages directory

--- pages/NewAndEdit/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-export function formatPrice(price) {
-    const priceWithoutSymbols = price.replace('R$ ', '');
-  
-    const priceWithDot = priceWithoutSymbols.replace(',', '.');
-  
-    return priceWithDot;
-  }
