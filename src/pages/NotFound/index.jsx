e8ed1d3784b27d4547cd43e4d8786176db3d0179
cconfort <113508854+csconfort1@users.noreply.github.com>
2023-12-19 16:54:11 +0000

    Add files via upload

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,4 +1,4 @@
-import {FiHome} from 'react-icons/fi';
+import {IoHomeOutline} from "react-icons/io5";
 import {useNavigate} from 'react-router-dom';
 
 import {Button} from '../../components/Button';
@@ -21,7 +21,7 @@ export function NotFound() {
         <p>Desculpe, a página que você buscou está indisponível.</p>
         <Button
           title="Voltar para Página Inicial"
-          icon={FiHome}
+          icon={IoHomeOutline}
           onClick={handleBack}
         />
       </main>
